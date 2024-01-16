e8c1679f9baf0181efc18dd4c4c7e97a7f943fee
Bobby Steckline <rjsturing@gmail.com>
2024-01-16 13:10:13 -0500

    Add form / POST test

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -13,27 +13,79 @@ describe("URL Shortener", () => {
     cy.get("form input").should("have.length", 2);
     cy.get("form button").should("exist");
 
-    cy.get(".url").should("have.length", 2); 
-    cy.get(".url").first().within(() => {
-      cy.get("h3").should("contain", "Awesome photo");
-      cy.get("a").should("contain", "http://localhost:3001/useshorturl/1");
-      cy.get("p").should("contain", "https://images.unsplash.com/photo-1531898418865-480b7090470f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80");
-    });
-    cy.get(".url").last().within(() => {
-      cy.get("h3").should("contain", "Bird");
-      cy.get("a").should("contain", "http://localhost:3001/useshorturl/2");
-      cy.get("p").should("contain", "https://media.audubon.org/2022-09/stay_abreast_bird.png");
-    });
+    cy.get(".url").should("have.length", 2);
+    cy.get(".url")
+      .first()
+      .within(() => {
+        cy.get("h3").should("contain", "Awesome photo");
+        cy.get("a").should("contain", "http://localhost:3001/useshorturl/1");
+        cy.get("p").should(
+          "contain",
+          "https://images.unsplash.com/photo-1531898418865-480b7090470f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80"
+        );
+      });
+    cy.get(".url")
+      .last()
+      .within(() => {
+        cy.get("h3").should("contain", "Bird");
+        cy.get("a").should("contain", "http://localhost:3001/useshorturl/2");
+        cy.get("p").should(
+          "contain",
+          "https://media.audubon.org/2022-09/stay_abreast_bird.png"
+        );
+      });
   });
 
   it("should have a form that takes input values", () => {
     cy.visit("http://localhost:3000/");
     cy.wait("@getUrls");
 
-    cy.get('input[name="title"]').type("Dog");
-    cy.get('input[name="urlToShorten"]').type("https://ireallylovedogstheyrethebest.com");
-    cy.get('input[name="title"]').should("have.value", "Dog");
-    cy.get('input[name="urlToShorten"]').should("have.value", "https://ireallylovedogstheyrethebest.com");
-  });  
+    cy.get("form button").should("exist").and("contain", "Shorten Please!");
 
+    cy.get('input[name="title"]').type("Dog");
+    cy.get('input[name="urlToShorten"]').type(
+      "https://ireallylovedogstheyrethebest.com"
+    );
+    cy.get('input[name="title"]').should("have.value", "Dog");
+    cy.get('input[name="urlToShorten"]').should(
+      "have.value",
+      "https://ireallylovedogstheyrethebest.com"
+    );
+  });
+
+  it("should add a new url and verify the button's text and functionality", () => {
+    cy.visit("http://localhost:3000/");
+    cy.wait("@getUrls");
+
+    cy.get("form button").should("exist").and("contain", "Shorten Please!");
+
+    cy.get('input[name="title"]').type("New Title");
+    cy.get('input[name="urlToShorten"]').type(
+      "https://reallywildlycrazydumblongurl.com"
+    );
+
+    cy.intercept("POST", "http://localhost:3001/api/v1/urls", {
+      statusCode: 201,
+      body: {
+        id: 3,
+        title: "New Title",
+        short_url: "http://localhost:3001/useshorturl/3",
+        long_url: "https://reallywildlycrazydumblongurl.com",
+      },
+    }).as("postUrl");
+
+    cy.get("form button").click();
+    cy.wait("@postUrl");
+
+    cy.get(".url").should("have.length", 3);
+    cy.get(".url h3").last().should("contain", "New Title");
+    cy.get(".url a")
+      .last()
+      .should("contain", "http://localhost:3001/useshorturl/3");
+    cy.get(".url p")
+      .last()
+      .should("contain", "https://reallywildlycrazydumblongurl.com");
+  });
+
+  
 });

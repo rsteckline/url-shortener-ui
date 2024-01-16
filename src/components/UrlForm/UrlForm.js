a622f8cf78154d43466f4303530ab97e5704e7f4
Bobby Steckline <rjsturing@gmail.com>
2024-01-16 12:37:39 -0500

    Add POST functionality

--- src/components/UrlForm/UrlForm.js
+++ src/components/UrlForm/UrlForm.js
@@ -1,42 +1,48 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import { postUrl } from "../../apiCalls";
 
-function UrlForm() {
+function UrlForm({ onUrlAdded }) {
   const [title, setTitle] = useState('');
   const [urlToShorten, setUrlToShorten] = useState('');
 
   const handleSubmit = e => {
     e.preventDefault();
-    clearInputs();
+    postUrl(urlToShorten, title)
+      .then(newUrl => {
+        onUrlAdded(newUrl);
+        clearInputs();
+      })
+      .catch(error => {
+        console.error('Error adding new URL:', error);
+      });
   }
 
   const clearInputs = () => {
-    setTitle('');
-    setUrlToShorten('');
-  }
+    setTitle("");
+    setUrlToShorten("");
+  };
 
   return (
     <form>
       <input
-        type='text'
-        placeholder='Title...'
-        name='title'
+        type="text"
+        placeholder="Title..."
+        name="title"
         value={title}
-        onChange={e => setTitle(e.target.value)}
+        onChange={(e) => setTitle(e.target.value)}
       />
 
       <input
-        type='text'
-        placeholder='URL to Shorten...'
-        name='urlToShorten'
+        type="text"
+        placeholder="URL to Shorten..."
+        name="urlToShorten"
         value={urlToShorten}
-        onChange={e => setUrlToShorten(e.target.value)}
+        onChange={(e) => setUrlToShorten(e.target.value)}
       />
 
-      <button onClick={e => handleSubmit(e)}>
-        Shorten Please!
-      </button>
+      <button onClick={(e) => handleSubmit(e)}>Shorten Please!</button>
     </form>
-  )
+  );
 }
 
 export default UrlForm;

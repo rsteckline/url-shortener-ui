f41f9db8e2ac538f11f4673f75cced7dc6a8cb15
Bobby Steckline <rjsturing@gmail.com>
2024-01-16 13:45:40 -0500

    Add form testing - Heather approved

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -87,5 +87,23 @@ describe("URL Shortener", () => {
       .should("contain", "https://reallywildlycrazydumblongurl.com");
   });
 
-  
-});
+  it("should display the correct number of URLs and verify the first and last URLs", () => {
+    cy.visit("http://localhost:3000/");
+    cy.wait("@getUrls");
+
+    cy.get(".url").should("have.length", 2);
+
+      cy.get(".url").first().within(() => {
+        cy.get("h3").should("contain", "Awesome photo");
+        cy.get("a").should("contain", "http://localhost:3001/useshorturl/1");
+        cy.get("p").should("contain", "https://images.unsplash.com/photo-1531898418865-480b7090470f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80");
+      });
+
+        cy.get(".url").last().within(() => {
+          cy.get("h3").should("contain", "Bird");
+          cy.get("a").should("contain", "http://localhost:3001/useshorturl/2");
+          cy.get("p").should("contain", "https://media.audubon.org/2022-09/stay_abreast_bird.png");
+        });
+    });
+
+  });
